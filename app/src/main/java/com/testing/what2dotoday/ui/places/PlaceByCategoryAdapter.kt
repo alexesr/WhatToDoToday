6a2e2462c5c9ee9ddcd623bd01e843f56b68ccfa
unknown <a01701357@itesm.mx>
2019-12-02 15:56:19 -0600

    Dialog working

--- app/src/main/java/com/testing/what2dotoday/ui/places/PlaceByCategoryAdapter.kt
+++ app/src/main/java/com/testing/what2dotoday/ui/places/PlaceByCategoryAdapter.kt
@@ -9,17 +9,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.testing.what2dotoday.R
 import com.testing.what2dotoday.SignInActivity
 import android.content.Intent
-import com.google.android.gms.location.LocationCallback
+import android.util.Log
+import com.testing.what2dotoday.MainActivity
 
 class PlaceByCategoryAdapter(val placesList: ArrayList<PlacesModel>): RecyclerView.Adapter<PlaceByCategoryAdapter.ViewHolder>(){
 
     private lateinit var myhandler: Handler
-    private  val splashTime = 500L
-    lateinit var locationCallback: LocationCallback
+    var flag : Boolean = false
+    private val exampleListFull: ArrayList<PlacesModel> = placesList
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.place_row ,parent,false)
         myhandler = Handler()
+        getFilter()
         return ViewHolder(v)
     }
 
@@ -36,10 +38,11 @@ class PlaceByCategoryAdapter(val placesList: ArrayList<PlacesModel>): RecyclerVi
         holder.placeLoc.text = "Near"
         holder.placePrice.text = row.price
         holder.write.setOnClickListener{
-
-            val context = holder.itemView.context
-            val intent = Intent(context, SignInActivity::class.java)
-            context.startActivity(intent)
+            //val context = holder.itemView.context
+            //val intent = Intent(context, SignInActivity::class.java)
+            //context.startActivity(intent)
+            val dialog = PlacesDialogFragment()
+            dialog.show(holder.activity!!.supportFragmentManager, "DeleteCategoryDialog")
         }
     }
 
@@ -51,6 +54,49 @@ class PlaceByCategoryAdapter(val placesList: ArrayList<PlacesModel>): RecyclerVi
         var placePrice =  itemView.findViewById(R.id.price) as TextView
 
         var write = itemView.findViewById(R.id.write) as ImageButton
+        var activity = itemView.context as? MainActivity
+
     }
 
+    fun getFilter(): Filter {
+        return exampleFilter
+    }
+
+    private val exampleFilter = object : Filter() {
+        override fun performFiltering(constraint: CharSequence?): Filter.FilterResults {
+            var filteredList : ArrayList<PlacesModel> = ArrayList()
+
+            val results = Filter.FilterResults()
+
+
+            if (constraint == null || constraint.isEmpty()) {
+                Log.i("debug", "Nada" )
+                filteredList.addAll(exampleListFull)
+                flag = false
+
+            } else {
+                val filterPattern = constraint.toString().toLowerCase().trim { it <= ' ' }
+
+                for (item in exampleListFull) {
+                    if (item.title.toLowerCase().contains(filterPattern)) {
+                        filteredList.add(item)
+                    }
+                    flag = true
+                }
+                Log.i("debug", "Algo" )
+
+            }
+//            filteredList.addAll(exampleListFull)
+
+            results.values = filteredList
+
+            return results
+        }
+
+        override fun publishResults(constraint: CharSequence, results: Filter.FilterResults) {
+            placesList.clear()
+            placesList.addAll(results.values as Collection<PlacesModel>)
+            notifyDataSetChanged()
+        }
+    }
 }
