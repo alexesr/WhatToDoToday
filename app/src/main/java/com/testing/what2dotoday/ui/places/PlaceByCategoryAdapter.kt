e6be66148dc09e428e881679ed8423be433997f5
unknown <a01701357@itesm.mx>
2019-12-01 21:22:31 -0600

    Add startActivity

--- app/src/main/java/com/testing/what2dotoday/ui/places/PlaceByCategoryAdapter.kt
+++ app/src/main/java/com/testing/what2dotoday/ui/places/PlaceByCategoryAdapter.kt
@@ -7,17 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.*
+import android.os.Handler
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.testing.what2dotoday.R
+import com.testing.what2dotoday.SignInActivity
 import kotlinx.android.synthetic.main.fragment_places.*
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import com.testing.what2dotoday.ui.test.TestAdapter
+import com.testing.what2dotoday.ui.test.TestModel
 import org.w3c.dom.Text
+import kotlin.random.Random
 
 
 class PlaceByCategoryAdapter(val placesList: ArrayList<PlacesModel>): RecyclerView.Adapter<PlaceByCategoryAdapter.ViewHolder>(){
 
+    private lateinit var myhandler: Handler
+    private  val splashTime = 500L
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.place_row ,parent,false)
+        myhandler = Handler()
         return ViewHolder(v)
     }
 
@@ -31,12 +42,21 @@ class PlaceByCategoryAdapter(val placesList: ArrayList<PlacesModel>): RecyclerVi
         holder.placeImage.setImageResource(row.image)
         holder.placeTitle.text = row.title
         holder.placeDescription.text = row.description
+
+        holder.write.setOnClickListener{
+            val context = holder.itemView.context
+            val intent = Intent(context, SignInActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         var placeImage = itemView.findViewById(R.id.place_image) as ImageView
         var placeTitle = itemView.findViewById(R.id.place_title) as TextView
         var placeDescription = itemView.findViewById(R.id.place_description) as  TextView
+
+        var write = itemView.findViewById(R.id.write) as ImageButton
+
     }
 
 }
--- app/src/main/java/com/testing/what2dotoday/ui/places/PlaceBySearchAdapter.kt
+++ app/src/main/java/com/testing/what2dotoday/ui/places/PlaceBySearchAdapter.kt
@@ -1,14 +1,17 @@
 package com.testing.what2dotoday.ui.places
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
 import com.testing.what2dotoday.R
 import android.util.Log
+import android.widget.Button
 
 
 class PlaceBySearchAdapter(val placesList: ArrayList<PlacesModel>): RecyclerView.Adapter<PlaceBySearchAdapter.ViewHolder>(){
@@ -20,8 +23,17 @@ class PlaceBySearchAdapter(val placesList: ArrayList<PlacesModel>): RecyclerView
         var placeImage = itemView.findViewById(R.id.place_image) as ImageView
         var placeTitle = itemView.findViewById(R.id.place_title) as TextView
         var placeDescription = itemView.findViewById(R.id.place_description) as  TextView
+
+        var write = itemView.findViewById(R.id.write) as ImageButton
     }
 
+    class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
+        init {
+            view.setOnClickListener{
+                println("Funciona")
+            }
+        }
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.place_row ,parent,false)
         return ViewHolder(v)
@@ -38,6 +50,11 @@ class PlaceBySearchAdapter(val placesList: ArrayList<PlacesModel>): RecyclerView
         holder.placeImage.setImageResource(row.image)
         holder.placeTitle.text = row.title
         holder.placeDescription.text = row.description
+
+        val context = holder.itemView.context
+        holder.itemView.setOnClickListener(){
+
+        }
     }
 
     fun getFilter(): Filter {
