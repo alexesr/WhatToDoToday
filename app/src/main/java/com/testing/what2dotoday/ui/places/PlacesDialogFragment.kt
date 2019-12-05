04b27b3b8d57ed6188d01305e497772ccdc69ffe
unknown <a01701357@itesm.mx>
2019-12-04 20:25:17 -0600

    Add Post review

--- app/src/main/java/com/testing/what2dotoday/ui/places/PlacesDialogFragment.kt
+++ app/src/main/java/com/testing/what2dotoday/ui/places/PlacesDialogFragment.kt
@@ -16,8 +16,21 @@ import android.view.View
 import android.view.Gravity
 import android.widget.*
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.place_row.*
+import kotlinx.android.synthetic.main.write_message.*
 import kotlinx.android.synthetic.main.write_message.view.*
+import kotlinx.android.synthetic.main.write_message.view.messageEt
+import org.json.JSONArray
+import org.json.JSONObject
+import org.w3c.dom.Text
+import java.net.HttpURLConnection
+import java.util.HashMap
 
 class PlacesDialogFragment : DialogFragment() {
 
@@ -30,9 +43,35 @@ class PlacesDialogFragment : DialogFragment() {
             // Inflate and set the layout for the dialog
             // Pass null as the parent view because its going in the dialog layout
             val view:View = inflater.inflate(com.testing.what2dotoday.R.layout.write_message,null)
+
             view.sendEmailBtn.setOnClickListener(){
-                sendInfo(view)
-                Log.e("Chale:","Just in the right place")
+                //sendInfo(view)
+
+                val message: String = view.messageEt.text.toString()
+                val subject: String = view.subjectEt.text.toString()
+
+                val body: Map<String, String> = java.util.HashMap<String, String>(2)
+
+                with(body as HashMap) {
+                    body["lugar"] = message
+                    body["texto"] = subject
+                }
+
+                val queue = Volley.newRequestQueue(this.context)
+                val url = "https://w2dt.herokuapp.com/resenas/"
+
+                // Request a string response from the provided URL.
+                val stringRequest = JsonObjectRequest(
+                    Request.Method.POST,
+                    url,
+                    JSONObject(body),
+                    Response.Listener<JSONObject> { response ->
+                        // Display the first 500 characters of the response string.
+                        Log.i("asd","Response is: $response")
+                    },
+                    Response.ErrorListener { error -> error.printStackTrace()})
+                queue.add(stringRequest)
+                queue.start()
             }
             builder.setView(view)
             builder.create()
